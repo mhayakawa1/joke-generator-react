5354ab505d113a0c1166852bf55f3b9a30fcb2ac
makihaya <93355988+makihaya@users.noreply.github.com>
2023-02-13 22:19:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App (){
   const [punchline, setPunchline] = useState('');
   const [show, setShow] = useState(false);
 
-
   const apiGet = () => {
     fetch('https://official-joke-api.appspot.com/random_joke')
       .then((response) => response.json())
@@ -20,28 +19,25 @@ function App (){
         setShow(false)
       })
       .catch(err=>console.log(err))
-  }
-
+  };
 
   const toggle = () => {
     setShow(true);
   };
 
-
   return(
-    <div>
+    <div className='container'>
       <h1>Random Joke Generator</h1>
+
       <div className='buttons'>
         <button onClick={apiGet}>New Joke</button>
         <button onClick={() => toggle()}>See Punchline</button>
       </div>
-     
-      <p className='setup'>{setup}</p>
 
-
-      {show === true ? (<p className='punchline'>{punchline}</p>) : (<p></p>)}
-
-
+      <div style={{height: '30%'}} className='text-container'>
+        <p className='setup'>{setup}</p>
+        {show === true ? (<p className='punchline'>{punchline}</p>) : (<p className='punchline'></p>)}      
+      </div>      
     </div>
   )
 }
